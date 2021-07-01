f04f612e040a7bf88c89ba8b068ab5428d49208a
Vlavimir Solodovnikov <svovanusus@inbox.ru>
2021-07-01 23:56:37 +0300

    updated database credentials

--- app/database.js
+++ app/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('postgres://shopping_list_app:application@localhost:5432/shopping_list');
+const connection = new Sequelize('postgres://mafamrftiyqadd:ca723d18d194c2907d0d541895ea5cb931da0efdcb9eb256fe888454a3b0833b@ec2-54-216-48-43.eu-west-1.compute.amazonaws.com:5432/d191qave0goesl');
 
 connection
   .authenticate()
